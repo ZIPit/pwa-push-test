6b851bb97e41fa00364805e222f24be4dc327cc6
Ivan Zubenko <ivzubenko@gmail.com>
2025-02-25 19:56:46 +0200

    your msg

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <div>
-        Hey! That's an app to test PWA Push w!       
+        Hey! That's an app to test PWA Push www!       
       </div>
       <PwaModal/>
       <Notification/> 
