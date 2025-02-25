7e2f007436fece555771c97074716ff6b99cc2b5
Ivan Zubenko <ivzubekno@gmail.com>
2025-02-25 19:44:56 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
       <div>
-        Hey! That's an app to test PWA Push!       
+        Hey! That's an app to test PWA Push w!       
       </div>
       <PwaModal/>
       <Notification/> 
